c6799d379dd65ebd63b8bc5228944b573d41dc47
GOATCHERKI <c.zikocherki@gmail.com>
2026-02-25 12:28:53 +0300

    inngest functions for workspace

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -51,5 +51,86 @@ const syncUserUpdate = inngest.createFunction(
   },
 );
 
+const workspaceCreation = inngest.createFunction(
+  { id: "workspace-creation", name: "Workspace Creation" },
+  { event: "workspace/created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.create({
+      data: {
+        id: data.id,
+        name: data.name,
+        slug: data.slug,
+        ownerId: data.created_by,
+        image_url: data.image_url,
+      },
+    });
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.created_by,
+        workspaceId: data.id,
+        role: "ADMIN",
+      },
+    });
+  },
+);
+
+const syncWorkspaceUpdate = inngest.createFunction(
+  { id: "sync-workspace-update", name: "Sync Workspace Update" },
+  { event: "workspace/updated" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        name: data.name,
+        slug: data.slug,
+        image_url: data.image_url,
+      },
+    });
+  },
+);
+
+const syncWorkspaceDeletion = inngest.createFunction(
+  { id: "sync-workspace-deletion", name: "Sync Workspace Deletion" },
+  { event: "workspace/deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.delete({
+      where: {
+        id: data.id,
+      },
+    });
+  },
+);
+
+const syncWorkspaceMemberAddition = inngest.createFunction(
+  {
+    id: "sync-workspace-member-addition",
+    name: "Sync Workspace Member Addition",
+  },
+  { event: "workspace.member.added" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.user_id,
+        workspaceId: data.workspace_id,
+        role: String(data.role).toUpperCase(),
+      },
+    });
+  },
+);
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate];
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdate,
+  workspaceCreation,
+  syncWorkspaceUpdate,
+  syncWorkspaceDeletion,
+  syncWorkspaceMemberAddition,
+];
