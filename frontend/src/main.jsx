b55b48422eb123b3b631de1bd504266bd357d959
GOATCHERKI <c.zikocherki@gmail.com>
2026-02-25 09:53:16 +0300

    editing frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,22 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
             <Provider store={store}>
-                <App />
+               <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                    <App />
+                </ClerkProvider>
             </Provider>
     </BrowserRouter>,
 )
