ddccc4ad688bec9614839ddfbf0911680465ebfd
GOATCHERKI <c.zikocherki@gmail.com>
2026-03-04 00:08:24 +0300

    Revert "invitation email"
    This reverts commit 6618002122e9216aa1c826532ddeadf8ca6c1f43.

--- backend/routes/workspaceRoutes.js
+++ backend/routes/workspaceRoutes.js
@@ -2,13 +2,11 @@ import express from "express";
 import {
   addUserToWorkspace,
   getUserWorkspaces,
-  sendWorkspaceInviteEmail,
 } from "../controllers/workspaceController.js";
 
 const workspaceRouter = express.Router();
 
 workspaceRouter.get("/", getUserWorkspaces);
 workspaceRouter.post("/add-member", addUserToWorkspace);
-workspaceRouter.post("/send-invite", sendWorkspaceInviteEmail);
 
 export default workspaceRouter;
